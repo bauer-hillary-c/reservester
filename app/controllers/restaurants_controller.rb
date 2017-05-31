895bfd752c5fb2fc313ba18e20f0d98106ed51c7
bauer-hillary-c <hbauer@ezcater.com>
2017-05-31 18:12:08 -0400

    adds restaurant resources, and new restaurant flow

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    @restaurant = Restaurant.new(params.require(:restaurant).permit(:name, :address, :food_type, :phone_number))
+
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
 end
