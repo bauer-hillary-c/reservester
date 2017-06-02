0fc2d4732bcd25be6b00cf33227e405bf361df82
bauer-hillary-c <hbauer@ezcater.com>
2017-06-01 21:02:07 -0400

    fixes paperclip :D

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,4 @@
 class Restaurant < ApplicationRecord
-  belongs_to :category
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
