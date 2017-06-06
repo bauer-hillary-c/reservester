b225a87c266cf0e12bd9cf90b3c6f54102c7be92
bauer-hillary-c <hbauer@ezcater.com>
2017-06-06 19:41:29 -0400

    creates local registrations controller and whitelists owner.name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :owners
+  devise_for :owners, :controllers => { registrations: 'registrations' }
   root "welcome#index"
   resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
