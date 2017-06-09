81fde36ac03da9d9bf7d6323250d4d52aa94fd18
bauer-hillary-c <hbauer@ezcater.com>
2017-06-08 21:33:33 -0400

    fixes food_type bug

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true, length: {minimum: 2}
   validates :address, presence: true, length: {minimum: 10}
   validates :phone_number, presence: true, length: {minimum: 10}
-  validates :genre, presence: true, length: {minimum: 2}
+  validates :food_type, presence: true, length: {minimum: 2}
 
   geocoded_by :address
   after_validation :geocode
