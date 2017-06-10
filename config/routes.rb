331ba5a91e770cd30a15eb6d1ccf1ba081ccd5b2
bauer-hillary-c <hbauer@ezcater.com>
2017-06-10 00:30:43 -0400

    adds nested reservation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :owners, :controllers => { registrations: 'registrations' }
   root "welcome#index"
-  resources :restaurants
+  resources :restaurants do
+    resources :reservations
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
