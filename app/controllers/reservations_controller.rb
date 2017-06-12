0cf8e1c9f213e5b6831c1f3d51425544c632dd22
bauer-hillary-c <hbauer@ezcater.com>
2017-06-12 17:29:35 -0400

    creates reversations controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,51 @@
+class ReservationsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      redirect_to @reservation
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def index
+    @reservations = Reservation.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @reservation.update(reservation_params)
+      redirect_to @reservation
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @reservation.destroy
+    render 'index'
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:restaurant_id, :event_at, :guest_name, :notes)
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+end
+
+end
