52bc09abe8c6abef29d98ecb751c7149ab8285cd
bauer-hillary-c <hbauer@ezcater.com>
2017-06-13 19:50:45 -0400

    generates OwnerMailer and creates new_reservation_email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,17 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def new_reservation_email(reservation)
+    @reservation = reservation
+    @restaurant = reservation.restaurant
+    @owner = @restaurant.owner
+
+    @url = 'http://localhost:3000/restaurants/<%= @restaurant.id %>/reservations/<%= @reservation.id %>'
+
+    mail(
+      to: "hbauer@localhost" # would be in prod @owner.email,
+      subject: 'New Reservation!'
+      )
+  end
 end
