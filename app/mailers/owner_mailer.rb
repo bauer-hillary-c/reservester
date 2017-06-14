1ad67d20a5c0f48cb9d232276ada9c22abf7c315
bauer-hillary-c <hbauer@ezcater.com>
2017-06-13 20:42:13 -0400

    corrects string interpolation and styling in OwnerMail

--- app/mailers/owner_mailer.rb
+++ app/mailers/owner_mailer.rb
@@ -1,16 +1,16 @@
 class OwnerMailer < ApplicationMailer
   default from: 'from@example.com'
-  
+
   def new_reservation_email(reservation)
     @reservation = reservation
     @restaurant = reservation.restaurant
     @owner = @restaurant.owner
 
-    @url = 'http://localhost:3000/restaurants/<%= @restaurant.id %>/reservations/<%= @reservation.id %>'
+    @url = 'http://localhost:3000/restaurants/#{@restaurant.id}/reservations/#{@reservation.id}'
 
     mail(
-      to: "hbauer@localhost" # would be in prod @owner.email,
-      subject: 'New Reservation!'
+      to: "hbauer@localhost", # would be in prod @owner.email,
+      subject: "New Reservation!"
       )
   end
 end
