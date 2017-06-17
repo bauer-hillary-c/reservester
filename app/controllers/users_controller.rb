d7cde80bfb3cd13c495493c4d3948fc164081f36
bauer-hillary-c <hbauer@ezcater.com>
2017-06-17 15:28:29 -0400

    sets up dashboard action in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :find_user
+
+  def dashboard
+    @reservations = Reservation.for_user(user_id: @user.id)
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
 end
