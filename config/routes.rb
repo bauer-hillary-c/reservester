2e4a467c11669cd4641c6b7e3c09429cbbcb0d33
bauer-hillary-c <hbauer@ezcater.com>
2017-06-17 12:16:50 -0400

    adds owner dashboard link to homepage, creates dashboard page, fixes owner_dashboard_path

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
     resources :reservations
   end
 
-  get "owners/:id/dashboard", to: "owners#dashboard", as: "owner_dashboard_path"
+  get "owners/:owner_id/dashboard", to: "owners#dashboard", as: "owner_dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
