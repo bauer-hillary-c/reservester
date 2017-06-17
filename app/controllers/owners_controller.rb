e02d09fdcd7aaac137e2cc77a249a74387c41952
bauer-hillary-c <hbauer@ezcater.com>
2017-06-17 10:19:31 -0400

    adds dashboard method to owners_controller and custom owner_dashboard_path

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,13 @@
 class OwnersController < ApplicationController
+  before_action :find_owner
+
+  def dashboard
+    @restaurants = @owner.restaurants
+  end
+
+  private
+
+  def find_owner
+    @owner = Owner.find(params[:owner_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :owners, :controllers => { registrations: 'registrations' }
+
   root "welcome#index"
+
   resources :restaurants do
     resources :reservations
   end
+
+  get "owners/:id/dashboard", to: "owners#dashboard", as: "owner_dashboard_path"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
