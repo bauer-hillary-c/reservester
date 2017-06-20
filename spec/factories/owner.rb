c04fa7ecc62a5267a3bd4fa30dd2d889128be128
bauer-hillary-c <hbauer@ezcater.com>
2017-06-20 13:27:18 -0400

    sets up owner factory

---
+++ spec/factories/owner.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  sequence :email do |n|
+    "person#{n}@example.org"
+  end
+end
+
+FactoryGirl.define do
+  factory :owner, class: 'Owner' do
+    name: 'Test McTester'
+    email
+    password '12345678'
+    password_confirmation '12345678'
+  end
+end
