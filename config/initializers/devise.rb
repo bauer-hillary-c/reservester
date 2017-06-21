7a4288719b19cb6627c1af67beb5fa0035295765
bauer-hillary-c <hbauer@ezcater.com>
2017-06-20 21:44:19 -0400

    adds facebook info to devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.omniauth :facebook, "1906300796259833", "734f3223b446f3993b023dedf3d6e65c", callback_url: "http://localhost:3000/users/auth/facebook/callback"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
